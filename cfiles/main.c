cdea83e1ebc91ad4f4a8d042d38d53c3d51ea7a5
Artur Kuznetsou <arturkuznetsou@pm.me>
2020-06-08 12:34:40 +0200

    Changed defaults

--- cfiles/main.c
+++ cfiles/main.c
@@ -20,7 +20,7 @@ int cursorIndex = 0;
 char* nextPage;
 char* prevPage;
 char* formatString;
-char* defaultFormatString = "$BROWSER %s&disown";
+char* defaultFormatString = "setsid -f $BROWSER %s";
 char* linkString = "https://www.youtube.com/watch?v=%s";
 
 struct vidBuf buffer;
